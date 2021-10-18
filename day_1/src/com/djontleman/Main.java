3f9d6317121e5384cab7160df1f557c148a4fb5b
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-10-18 15:55:03 +0100

    Added comments on basic setup and reserved keywords

--- day_1/src/com/djontleman/Main.java
+++ day_1/src/com/djontleman/Main.java
@@ -1,6 +1,9 @@
 // When using Java Project SDK
 package com.djontleman;
 
+// IntelliJ auto-saves
+
+// Class names should be capitalised
 public class Main {
 
     // Must have public static void and within a class
@@ -13,7 +16,20 @@ public class Main {
         System.out.print("Hello World! ");
         System.out.println("(Not you Steve)");
 
+        // Run by clicking green arrows
+        // Generates configuration is not already generated
+
+        // Running from terminal requires adding to PATH
+        // Todo: Add intelliJ to PATH
+
+        /* Java has reserved keywords
+        These cannot be used for variables, methods, classes,
+        or any other identifiers */
 
         
+
+
+
+
     }
 }
