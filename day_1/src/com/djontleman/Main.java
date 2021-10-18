7557149fc8ea65b84dbaba9524df40175c1f7295
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-10-18 15:42:25 +0100

    New HelloWorld Project in day_1 folder

---
+++ day_1/src/com/djontleman/Main.java
@@ -0,0 +1,19 @@
+// When using Java Project SDK
+package com.djontleman;
+
+public class Main {
+
+    // Must have public static void and within a class
+    public static void main(String[] args) {
+
+         /* Prints to console
+         .print() does not start new line after
+         .println() starts new line after
+         Must use double quotes ("") */
+        System.out.print("Hello World! ");
+        System.out.println("(Not you Steve)");
+
+
+        
+    }
+}
