b73c4f711c31956ba2197a5838e4a23cee2abaed
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-10-19 16:54:18 +0100

    Initialized day_2 project and added topics covered

---
+++ day_2/src/com/djontleman/Main.java
@@ -0,0 +1,32 @@
+package com.djontleman;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* Java Day 2
+        Topics Covered: amigoscode/bright/day-1
+        * Variables
+            * Primitives
+            * Arrays
+            * Reference Types
+                * Strings
+                * String Equality
+                * Points
+        * Operators
+            * Arithmetic
+            * Increments
+            * Comparison
+            * Logical
+        * Loops
+            * for and while
+            * break, continue and return
+        * Packages
+            * package and import
+        * Conditions
+            * if, else and else if
+            * switch
+        * User Input
+            * Scanners */
+    }
+}
