2b4a0c6c515b3b8cdbb3b3e0f71607bcef004b53
Djontleman <jhutchinson531@gmail.com>
2021-10-26 20:53:07 +0100

    Make UserDOA interface

---
+++ day_7/src/com/djontleman/UserDAO.java
@@ -0,0 +1,15 @@
+package com.djontleman;
+
+import com.djontleman.person.Person;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    List<Person> getUsers();
+
+//    int addUser(Person user);
+//    int removeUser(Person user);
+//
+//    Person getUser();
+}
