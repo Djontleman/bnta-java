da74ae7f721052e4e1273821417a5f6e1f3c4e47
Djontleman <jhutchinson531@gmail.com>
2021-10-28 10:59:32 +0100

    Initial MyFirstTest class

---
+++ day_9/src/main/java/MyFirstTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MyFirstTest {
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void test2() {
+        assertEquals(1, 1);
+    }
+}
