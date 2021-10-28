f86accfa527981727244b81aa2b8a701fea32b2c
Djontleman <jhutchinson531@gmail.com>
2021-10-28 13:55:46 +0100

    Make exercise package with exercise description

---
+++ day_9/src/main/java/com/djontleman/testing/exercise/ExerciseMain.java
@@ -0,0 +1,27 @@
+package com.djontleman.testing.exercise;
+
+public class ExerciseMain {
+
+    /* - Create gender enum
+       - Create a class Person with Name and Gender
+       - Take the following imperative code and turn into declarative using Streams.
+
+        List<Person> people = List.of(
+                new Person("Smith", Gender.FEMALE),
+                new Person("Anna", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Oliver", Gender.MALE)
+        );
+
+        List<Person> female = new ArrayList<>();
+
+        people.forEach(person -> {
+            var isGender = person.gender.equals(Gender.FEMALE);
+            if (isGender) {
+                female.add(person);
+            }
+        });
+
+        female.forEach(System.out::println); */
+
+}
